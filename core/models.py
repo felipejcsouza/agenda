8c69df87a8b965c0df055c4a4f388f5da9cd25b4
felipejcs <felipejcs@gmail.com>
2020-05-25 17:31:30 -0300

    Criado tela de listagem de eventos

--- core/models.py
+++ core/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+
 # Create your models here.
 
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
+    local = models.CharField(max_length=100)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateTimeField(auto_now=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -15,4 +18,5 @@ class Evento(models.Model):
     def __str__(self):
         return self.titulo
 
-
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M Hrs')
--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+from core.models import Evento
+
+
+def eventos(request, titulo_evento):
+    evento=Evento.objects.get(titulo=titulo_evento)
+    return HttpResponse(evento.local)
+
+
+def listaeventos(request):
+    usuario = request.user
+    evento = Evento.objects.all()
+    dados = {'eventos':evento,}
+    return render(request, 'agenda.html', dados)
+
+
+# def index(request):
+#     return redirect('/agendas/')
